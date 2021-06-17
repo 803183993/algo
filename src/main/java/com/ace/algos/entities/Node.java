1153043a9f0b1689ecb1109fb35cc5ab5bb3a45d
amdad.choudhury <amdad.choudhury@outlook.com>
2021-06-17 18:45:10 +0100

    create project.

---
+++ src/main/java/com/ace/algos/entities/Node.java
@@ -0,0 +1,71 @@
+package com.ace.algos.entities;
+
+import java.util.*;
+
+public class Node<T>
+{
+    private final T value;
+    private final Set<Node<T>> children;
+
+    public Node(T value)
+    {
+        this.value = value;
+        this.children = new HashSet<>();
+    }
+
+    public T getValue()
+    {
+        return value;
+    }
+
+    public Set<Node<T>> getChildren()
+    {
+        return children;
+    }
+
+    public void connect(Node<T> node)
+    {
+        if (this == node) throw new IllegalArgumentException("Can't connect node to itself");
+        this.children.add(node);
+        node.children.add(this);
+    }
+
+    /**
+     * It adds a node to the queue at most once, therefore visiting it at most once also.
+     * Thus, if n is the number of nodes in the tree, the time complexity of the algorithm will be O(n).
+     * Now, for the graph algorithm, things are a little bit more complicated. We'll go through each node
+     * at most once, but to do so we'll make use of operations having linear-complexity such as addAll() and removeAll().
+     * Let's consider n the number of nodes and c the number of connections of the graph. Then, in the worst case (being
+     * no node found), we might use addAll() and removeAll() methods to add and remove nodes up to the number of
+     * connections, giving us O(c) complexity for these operations. So, provided that c > n, the complexity of the overall
+     * algorithm will be O(c). Otherwise, it'll be O(n). This is generally noted O(n + c), which can be interpreted as a
+     * complexity depending on the greatest number between n and c.
+     */
+    public static <T> Optional<Node<T>> breadthFirstSearch(T value, Node<T> start)
+    {
+        Queue<Node<T>> queue = new ArrayDeque<>();
+        queue.add(start);
+
+        Node<T> currentNode;
+
+        Set<Node<T>> alreadyVisited = new HashSet<>();
+        while (!queue.isEmpty())
+        {
+            currentNode = queue.remove();
+            System.out.println("Visited node with value: {}" + currentNode.getValue());
+
+            if (currentNode.getValue().equals(value))
+            {
+                return Optional.of(currentNode);
+            }
+            else
+            {
+                alreadyVisited.add(currentNode);
+                queue.addAll(currentNode.getChildren());
+                queue.removeAll(alreadyVisited);
+            }
+        }
+
+        return Optional.empty();
+    }
+}
--- src/test/java/com/ace/algos/SearchTest.java
+++ src/test/java/com/ace/algos/SearchTest.java
@@ -1,5 +1,6 @@
 package com.ace.algos;
 
+import com.ace.algos.entities.Node;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,6 +9,26 @@ import static org.hamcrest.Matchers.is;
 public class SearchTest
 {
     private static final int[] UNSORTED_NUMBERS = new int[]{10, 17, 13, 15, 2, 11, 16, 10, 14, 18};
+
+    @Test
+    public void shouldReturnSpecifiedNumberFromTreeUsingBreadthFirstSearch()
+    {
+        Node<Integer> start = new Node<>(10);
+        Node<Integer> firstChild = new Node<>(2);
+        start.connect(firstChild);
+
+        Node<Integer> childOfFirstChild = new Node<>(3);
+        firstChild.connect(childOfFirstChild);
+        childOfFirstChild.connect(start);
+
+        Node<Integer> secondChild = new Node<>(4);
+        start.connect(secondChild);
+
+        //noinspection OptionalGetWithoutIsPresent
+        assertThat(Node.breadthFirstSearch(4, start).get().getValue(), is(secondChild.getValue()));
+        //noinspection OptionalGetWithoutIsPresent
+        assertThat(Node.breadthFirstSearch(3, secondChild).get().getValue(), is(childOfFirstChild.getValue()));
+    }
 
     @Test
     public void shouldUseLinearSearchToFindSpecifiedNumber()
